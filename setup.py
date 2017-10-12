aa5a8ee536d87ea972822ab58bbd357fbcc91839
Andy Idiaghe <andy@getlivesmart.com>
2017-10-12 11:16:23 +0100

    Adds basic setup file and main dir

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+	name='CLI language detector',
+	version='0.0.1',
+	description='A command line utility',
+	author='Andy Idiaghe',
+	packages=['langdetector'],
+	)
